c473838d188dba52a1b21c6876d81ba6a874ff4c
Daniil <daniil_chuvashev@internet.ru>
2025-12-18 15:07:48 +0400

    add menu

--- Progenj_Kursovoi_Fertikov_Konnikov_Chuvashev/Models.cs
+++
@@ -1,6 +0,0 @@
-﻿public class Hero
-{
-    public string Name { get; set; }
-    public Dictionary<string, double> Advantage { get; set; } = new();
-    public Dictionary<string, double> Synergy { get; set; } = new();
-}
--- Progenj_Kursovoi_Fertikov_Konnikov_Chuvashev/Repository.cs
+++
@@ -1,7 +0,0 @@
-﻿public class HeroRepository
-{
-    private readonly Dictionary<string, Hero> _heroes = new();
-
-    public void AddHero(Hero hero) => _heroes[hero.Name] = hero;
-    public IEnumerable<Hero> All => _heroes.Values;
-}
