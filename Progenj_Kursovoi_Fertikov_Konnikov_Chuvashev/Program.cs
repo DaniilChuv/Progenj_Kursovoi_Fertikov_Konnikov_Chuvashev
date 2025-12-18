c72a0e022f6b37e1a2369616192e066e556e87a9
Daniil <daniil_chuvashev@internet.ru>
2025-12-18 14:54:26 +0400

    GlobalUsings, Models,Program

--- Progenj_Kursovoi_Fertikov_Konnikov_Chuvashev/Program.cs
+++ Progenj_Kursovoi_Fertikov_Konnikov_Chuvashev/Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        var repo = DemoData.CreateRepository();
+        var service = new CounterPickService(repo);
+
+        var enemies = new List<string>();
+        var allies = new List<string>();
+        var bans = new List<string>();
+
+        while (true)
+        {
+            var available = repo.All
+                .Where(h => !enemies.Contains(h.Name)
+                         && !allies.Contains(h.Name)
+                         && !bans.Contains(h.Name))
+                .ToList();
+
+            ConsoleUI.DrawScreen(available, enemies, allies, bans);
+
+            var key = Console.ReadKey(true).KeyChar;
+
+            if (key == '0')
+                break;
+
+            if (key == '4')
+            {
+                var scores = service.CalculateScores(enemies, allies, bans);
+                ConsoleUI.ShowRecommendations(scores);
+                continue;
+            }
+
+            if (key == '5')
+            {
+                BanRecommendation.Show();
+                continue;
+            }
+
+            if (key == '6')
+            {
+                MatchGameRecommendation.Show();
+                continue;
+            }
+
+            if (key is '1' or '2' or '3')
+            {
+                if (available.Count == 0)
+                    continue;
+
+                int index = ConsoleUI.AskHeroIndex(available.Count);
+                if (index == -1)
+                    continue;
+
+                var heroName = available[index].Name;
+
+                if (key == '1') enemies.Add(heroName);
+                if (key == '2') allies.Add(heroName);
+                if (key == '3') bans.Add(heroName);
+            }
+        }
+    }
+}
+
